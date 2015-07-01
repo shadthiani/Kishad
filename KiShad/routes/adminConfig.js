6d065b1b6f951577afc0b49d26b0116d5cabaf4d
shadthiani <mutiesm@gmail.com>
2015-07-01 22:07:49 +0300

    changes

---
+++ KiShad/routes/adminConfig.js
@@ -0,0 +1,45 @@
+﻿function adminConfig(app){
+    this.app = app;
+    this.routeTable = [];
+    this.init();
+}
+
+adminConfig.prototype.init = function (){
+    var self = this;
+    this.addRoutes();
+    this.processRoutes();
+}
+
+adminConfig.prototype.addRoutes = function () {
+    var self = this;
+    
+    self.routeTable.push({
+        requestType: 'get',       
+        requestUrl: '/resultLanguage',
+        callbackFunction: function (req, res) {
+            res.render('resultLanguage', { title: "Add result language" })
+        }
+    });
+
+    self.routeTable.push({
+        requestType: 'get',       
+        requestUrl: '/projectType',
+        callbackFunction: function (req, res){
+            res.render('projectType', {title:"Add project type"})
+        }
+    });
+
+}
+adminConfig.prototype.processRoutes = function () {
+    var self = this;
+    self.routeTable.forEach(function (route) {
+        if (route.requestType == 'get') {
+            self.app.get(route.requestUrl,route.callbackFunction);
+        }
+        else if (route.requestType == 'post') { }
+        else if (route.requestType == 'delete') { }
+       
+    });
+}
+module.exports = adminConfig;
+
